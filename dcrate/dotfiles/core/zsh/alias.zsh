8b5a18bc151aefe6df82ec37a117fd82fd60cddf
Zhu Zhan Yan <program.nom@gmail.com>
2019-10-13 12:13:54 +0800

    Extracted out zsh aliases in alias.zsh

---
+++ dcrate/dotfiles/core/zsh/alias.zsh
@@ -0,0 +1,22 @@
+#
+# Devcrate
+# ZSH Aliases
+#
+
+
+# Aliases
+alias ls='ls -F --color=none'
+alias md="mkdir -p"
+alias vim="nvim"
+alias v="nvim"
+alias py="python3"
+alias rb="ruby"
+alias g="git"
+alias gl="git log --pretty=oneline --graph"
+alias tmux="tmux new-session -A -s $HOST"
+alias t="tmux"
+alias less="less -R"
+alias n="nautilus"
+alias r="ranger"
+alias k="kubectl"
+alias mk="microk8s.kubectl"
