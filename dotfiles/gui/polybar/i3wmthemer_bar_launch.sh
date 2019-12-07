22ee66ebe97a09acdbcadda815ae9e1ec30c668b
Zhu Zhan Yan <program.nom@gmail.com>
2019-12-07 13:03:50 +0800

    Configure startup script to automatically detect display and place polybar

--- dotfiles/gui/polybar/i3wmthemer_bar_launch.sh
+++ dotfiles/gui/polybar/i3wmthemer_bar_launch.sh
@@ -8,5 +8,7 @@ sleep 1;
 export ETHERNET_INTERFACE=$(ip l | grep ' en' | sed -e 's/://g'  | awk '{print $2}')
 export WLAN_INTERFACE=$(ip l | grep ' wl' | sed -e 's/://g'  | awk '{print $2}')
 
-polybar DP  &
-polybar HDMI  &
+for MONITOR in $(xrandr -q | grep " connected" | cut -d ' ' -f1)
+do
+    env MONITOR="$MONITOR" polybar master &
+done
