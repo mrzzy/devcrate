7d859f16d3007ecc91e23f076c354c7f81416c86
Zhu Zhan Yan <program.nom@gmail.com>
2019-12-07 12:33:29 +0800

    Added polybar support for up to 2 displays

--- dotfiles/gui/polybar/i3wmthemer_bar_launch.sh
+++ dotfiles/gui/polybar/i3wmthemer_bar_launch.sh
@@ -8,4 +8,5 @@ sleep 1;
 export ETHERNET_INTERFACE=$(ip l | grep ' en' | sed -e 's/://g'  | awk '{print $2}')
 export WLAN_INTERFACE=$(ip l | grep ' wl' | sed -e 's/://g'  | awk '{print $2}')
 
-polybar i3wmthemer_bar &
+polybar DP  &
+polybar HDMI  &
