f1ff7199b7931a8633eec2e30644d976ffee19d6
Zhu Zhanyan <program.nom@gmail.com>
2021-03-25 10:24:24 +0800

    zsh: Add aliases for flatpack CLI
    Signed-off-by: Zhu Zhanyan <program.nom@gmail.com>

--- dotfiles/base/zsh/alias.zsh
+++ dotfiles/base/zsh/alias.zsh
@@ -23,24 +23,26 @@ alias tf="terraform"
 alias g="git"
 alias gvm="/home/zzy/.local/go/bin/g"
 alias nvm="/home/zzy/.local/share/n/bin/n"
+alias fp="flatpak"
+alias fpr="flatpak run"
 
 ## virtual machine management
 ## vmv_shutdown(): shutdown vm of the given domain
-vmv_shutdown() 
+vmv_shutdown()
 {
     echo "Stopping VM: $VM_DOMAIN ..."
     if ! virsh shutdown "$VM_DOMAIN"
     then
         echo "Error: failed to stop VM $VM_DOMAIN"
     fi
-    
+
     # unhook trap on ctrl-c
     trap - SIGINT
 }
 
 # Alias Function
 # vmv(<domain>): starts vm of the given domain and attaches a viewer to it
-vmv() 
+vmv()
 {
     VM_DOMAIN="$1"
     echo "Starting VM $VM_DOMAIN..."
